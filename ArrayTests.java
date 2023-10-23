db26d37c3b5096242f5fb610f6a6776b1fe89b66
soccerplayer6 <32963392+soccerplayer6@users.noreply.github.com>
2023-10-23 16:34:46 -0700

    Fixed reverse functions

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,8 +8,21 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
-
+  
+  @Test
+  public void testRevInPlace()
+  {
+    int [] input = {1,2,3,4};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{4,3,2,1}, input);
+  }
+  
+  @Test
+  public void testRev()
+  {
+    int [] input = {1,2,3,4};
+    assertArrayEquals(new int[]{4,3,2,1},ArrayExamples.reversed(input));
+  }
   @Test
   public void testReversed() {
     int[] input1 = { };
